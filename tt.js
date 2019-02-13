4db3ee3a48ca27832e95b2a94e58314348c88e72
fakesoul <bharatraj.mulchandani@gmail.com>
2019-02-13 15:11:33 +0530

    First branch 1

---
+++ tt.js
@@ -0,0 +1,9 @@
+var person = new Object();
+
+person["fname"] = "bharat";
+person["lname"] = "mulchandani";
+
+var fnameProperty = "fname";
+
+// console.log(person);
+console.log(person[fnameProperty]);
